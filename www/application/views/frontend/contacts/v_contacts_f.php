19530cdaaff239b848b869c12c34d173437ad3b2
Mykola Pelekh <mpelekh@lohika.com>
2019-05-09 21:56:01 +0300

    Update google map to the correct adress

--- www/application/views/frontend/contacts/v_contacts_f.php
+++ www/application/views/frontend/contacts/v_contacts_f.php
@@ -7,7 +7,7 @@
         </div>
         <div class="row">
             <div class="col-xs-12 col-xs-offset-0 col-sm-12 col-sm-offset-0 col-md-5 col-md-offset-2 col-lg-5 col-lg-offset-2">
-                <iframe class="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1335.7131564022172!2d24.015584721506663!3d47.966816733776305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4737aff385e56f7b%3A0x974a96e94afcacc5!2z0JrQsNGA0L_QsNGC0LgsINCi0J7Qkg!5e0!3m2!1sru!2sua!4v1413831552595" width="100%" height="350" frameborder="0" style="border:0"></iframe>
+                <iframe class="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10692.111572239255!2d24.166363186082418!3d47.935844165691655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4737ad9ba1cc6cff%3A0x9bea79acaba5a42e!2z0JTQtdC70L7QstC-0LUsINCX0LDQutCw0YDQv9Cw0YLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgOTA2MjU!5e0!3m2!1sru!2sua!4v1557427945631!5m2!1sru!2sua" width="100%" height="350" frameborder="0" style="border:0"></iframe>
             </div>
             <div class="col-xs-12 col-xs-offset-0 col-sm-12 col-sm-offset-0 col-md-3 col-md-offset-0 col-lg-3 col-lg-offset-0" id="contacts-main-text-col">
 
